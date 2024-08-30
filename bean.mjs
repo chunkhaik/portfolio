d033c48c223380dd2039da3feae8152d6c6b4e6d
chun.kok <chun.kok@bytedance.com>
2024-08-30 11:27:20 +0800

    added formatting to bean page

--- bean.mjs
+++ bean.mjs
@@ -5,7 +5,7 @@ dotenv.config({ path: '.env.local' })
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_ANON
 )
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default async function HomePage() {
 	return (
-		<div className='container max-w-6xl pt-6 lg:py-10'>
+		<div className='container max-w-4xl py-6 lg:py-10'>
 			<AboutMe />
 		</div>
 	);
