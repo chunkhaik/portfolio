522e40d67311b693a0202d12528cf8f4349bdd5c
Chun Khai <chunkhai.k@gmail.com>
2024-09-24 21:09:05 +0800

    fix pnpm lock

--- src/components/grid-image.tsx
+++ src/components/grid-image.tsx
@@ -3,22 +3,22 @@
 import { SearchResult } from '@/components/photography-cloudinary';
 import { ReactNode } from 'react';
 
-const MAX_COLUMNS = 4;
-
 export function ImageGrid({
 	images,
 	getImage,
+	columns = 4,
 }: {
 	images: SearchResult[];
 	getImage: (imageData: SearchResult) => ReactNode;
+	columns?: number;
 }) {
 	function getColumns(colIndex: number) {
-		return images.filter((_, idx) => idx % MAX_COLUMNS === colIndex);
+		return images.filter((_, idx) => idx % columns === colIndex);
 	}
 
 	return (
-		<div className='grid grid-cols-4 gap-2'>
-			{[0, 1, 2, 3].map((colIndex) => (
+		<div className={`grid grid-cols-${columns} gap-2`}>
+			{Array.from({ length: columns }).map((_, colIndex) => (
 				<div key={colIndex} className='flex flex-col gap-2'>
 					{getColumns(colIndex).map(getImage)}
 				</div>
