2bb2023d8f245a644ad410c5278183aca807a549
Chun Khai <chunkhai.k@gmail.com>
2024-09-24 22:02:02 +0800

    fixed weird tailwind behaviour

--- src/components/grid-image.tsx
+++ src/components/grid-image.tsx
@@ -2,6 +2,7 @@
 
 import { SearchResult } from '@/components/photography-cloudinary';
 import { ReactNode } from 'react';
+import { cn } from '@/lib/utils';
 
 export function ImageGrid({
 	images,
@@ -13,13 +14,19 @@ export function ImageGrid({
 	isMainAlbum: boolean;
 }) {
 
+	const columns = isMainAlbum ? 4 : 2;
+	const gridClass = 'grid-cols-' + columns;
+
 	function getColumns(colIndex: number) {
 		return images.filter((_, idx) => idx % columns === colIndex);
 	}
-	const columns = isMainAlbum ? 4 : 2;
+
+	console.log(columns, gridClass);
 
 	return (
-		<div className={`grid grid-cols-${columns} gap-2`}>
+		<div className={cn(
+			'grid gap-2', gridClass
+		)}>
 			{Array.from({ length: columns }).map((_, colIndex) => (
 				<div key={colIndex} className='flex flex-col gap-2'>
 					{getColumns(colIndex).map(getImage)}
