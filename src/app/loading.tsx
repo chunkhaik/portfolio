7a88cbff4a0748eb2796b0696a557b0ad673f9ff
Chun Khai <chunkhai.k@gmail.com>
2024-09-24 23:12:04 +0800

    remove extra logs

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,6 @@
 import {LoadingSpinner } from '@/components/ui/loading-spinner'
 
 export default function Loading({ title }: { title: string }) {
-	
-	console.log(title)
-
 	return (
 		<div className='container max-w-4xl py-8 lg:py-10'>
 			<div className='flex flex-col justify-center items-center min-h-[150px]'>
