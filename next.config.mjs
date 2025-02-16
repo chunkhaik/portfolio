92c742e8371aaab6de6e0536d23116a637fd1d05
Chun Khai <chunkhai.k@gmail.com>
2025-02-17 00:13:37 +0800

    packages update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
-import { build } from 'velite'
-
 /** @type {import('next').NextConfig} */
 export default {
-  // othor next config here...
   webpack: config => {
-    config.plugins.push(new VeliteWebpackPlugin())
+    config.cache = false
     return config
   }, 
 
@@ -35,18 +32,3 @@ export default {
     ];
   },
 };
-
-class VeliteWebpackPlugin {
-  static started = false
-  apply (/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs
-    // twice for the server (nodejs / edge runtime) and once for the client
-    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-      if (VeliteWebpackPlugin.started) return
-      VeliteWebpackPlugin.started = true
-      const dev = compiler.options.mode === 'development'
-      await build({ watch: dev, clean: !dev })
-    })
-  }
-}
-
