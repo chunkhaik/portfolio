e1540036298ac9d5ace1cd8910e20e694ba939a7
Chun Khai <chunkhai.k@gmail.com>
2025-02-17 15:44:30 +0800

    small bug fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { Post }  from "#/site/content"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -12,13 +11,5 @@ export function formatDate(input: string | number) {
     month: 'long',
     day: 'numeric',
     year: 'numeric',
-  })
-}
-
-export function sortPostsByDate(posts: Array<Post>) {
-  return posts.sort((a, b) => {
-    if (a.date > b.date) return -1;
-    if (a.date < b.date) return 1;
-    return 0;
   })
 }
